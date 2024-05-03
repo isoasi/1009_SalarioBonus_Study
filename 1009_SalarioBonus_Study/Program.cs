fd84a8f8db2f81b6aa15d498a72a3a58e382969f
Paola <Paola@DESKTOP-L53JRFL>
2024-05-02 22:36:09 -0300

    Adicionar arquivos de projeto.

---
+++ 1009_SalarioBonus_Study/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Numerics;
+
+class URÌ
+{
+    static void Main(string[] args)
+    {
+        string nomeVendedor;
+        double salarioFixo, vendas, totalSalario;
+
+        nomeVendedor = Console.ReadLine();
+        salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        totalSalario = salarioFixo + (vendas * 0.15);
+        //It calculates the total amount to be received by adding the fixed salary to 15% of the total sales
+        //(vendas * 0.15 represents the commission).
+        
+        Console.WriteLine("TOTAL = R$ " + totalSalario.ToString("F2", CultureInfo.InvariantCulture));
+
+        Console.ReadLine();
+
+    }
+}
